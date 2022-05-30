b516a8205e415e815a84a24fa094913414ada19a
jiajun <eric1986_chn@yahoo.com>
2022-05-30 20:47:00 +0200

    Add sr to read file returns

--- pyspatools/fileio/reader.py
+++ pyspatools/fileio/reader.py
@@ -6,14 +6,30 @@ def read_file(path: str, bitrate: int = 24, transpose: bool = True):
     """
     Read audio source. Return a list of all channels
     TODO: Add support for other bitrate audio
+
+    Parameters
+    ----------
+    path : str
+        Path to audio source
+    bitrate : int
+        Bit rate
+    transpose : bool
+        Whether to apply transpose to the read file signal.
+
+    Returns
+    -------
+    list
+        A list of audio signal channels
+    sr : int
+        The sampling rate of the file
     """
     if bitrate==24:
         _dtype = 'int32'
     else:
         raise AttributeError('Unsupported bitrate, currently: 24')
-    source = soundfile.SoundFile(path).read(dtype=_dtype, always_2d=True)
+    source, sr = soundfile.read(file=path, dtype=_dtype, always_2d=True)
     if transpose:
         source = np.transpose(np.right_shift(source, 8))
 
-    return [ch for ch in source]
+    return [ch for ch in source], sr
 
