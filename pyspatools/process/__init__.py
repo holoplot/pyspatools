e1a9541f41a3ede8124a6c69cde443bf7199c6b0
jiajun <eric1986_chn@yahoo.com>
2022-06-03 11:06:55 +0200

    [process] Add Diff dataclass

--- pyspatools/process/__init__.py
+++ pyspatools/process/__init__.py
@@ -1,4 +1,6 @@
+from .diff import *
 from .spectral import *
 from .waveform import *
+
 
 __all__ = [s for s in dir() if not s.startswith('_')]
---
+++ pyspatools/process/diff.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+import numpy as np
+
+
+__all__ = ['Diff']
+
+
+@dataclass
+class Diff:
+    a: np.ndarray
+    b: np.ndarray
+    sr: int
+    tolerance: int = 0
+
+    @property
+    def delta(self) -> np.ndarray:
+        """Element differences between a and b"""
+        return self.a - self.b
+
+    @property
+    def where(self) -> list:
+        """A list of indices where the absolute delta value is greater than tolerance per channel."""
+        return [np.where(np.abs(ch_delta) > self.tolerance)[0] for ch_delta in self.delta]
+
+    @property
+    def channel_max(self) -> np.ndarray:
+        """An array of max value per channel"""
+        return np.amax(self.delta, axis=1)
+
+    @property
+    def channel_min(self) -> np.ndarray:
+        """An array of min value per channel"""
+        return np.amin(self.delta, axis=1)
+
+    @property
+    def channel_max_indices(self) -> list:
+        """A list with an array of each channel where the indices of channel_max value occurred"""
+        return [np.where(self.delta[i, :] == self.channel_max[i])[0] for i in range(len(self.channel_max))]
+
+    @property
+    def channel_min_indices(self) -> list:
+        """A list with an array of each channel where the indices of channel_min value occurred"""
+        return [np.where(self.delta[i, :] == self.channel_min[i])[0] for i in range(len(self.channel_min))]
