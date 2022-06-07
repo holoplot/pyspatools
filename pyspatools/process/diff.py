b7e0540eda686d30a594a000e309f78581a12b5f
jiajun <eric1986_chn@yahoo.com>
2022-06-07 12:08:25 +0200

    [process] diff bug fix, rm maxmin indices properties for now.

--- pyspatools/process/diff.py
+++ pyspatools/process/diff.py
@@ -13,6 +13,10 @@ class Diff:
     tolerance: int = 0
 
     @property
+    def channels(self):
+        return self.a.shape[0]
+
+    @property
     def delta(self) -> np.ndarray:
         """Element differences between a and b"""
         return self.a - self.b
@@ -25,20 +29,9 @@ class Diff:
     @property
     def channel_max(self) -> np.ndarray:
         """An array of max value per channel"""
-        return [np.where(np.abs(item) > self.tolerance)[0] for item in np.amax(self.delta, axis=1)]
+        return np.amax(self.delta, axis=1)
 
     @property
     def channel_min(self) -> np.ndarray:
         """An array of min value per channel"""
-        # return np.amin(self.delta, axis=1)
-        return [np.where(np.abs(item) > self.tolerance)[0] for item in np.amax(self.delta, axis=1)]
-
-    @property
-    def channel_max_indices(self) -> list:
-        """A list with an array of each channel where the indices of channel_max value occurred"""
-        return [np.where(self.delta[i, :] == self.channel_max[i])[0] for i in range(len(self.channel_max))]
-
-    @property
-    def channel_min_indices(self) -> list:
-        """A list with an array of each channel where the indices of channel_min value occurred"""
-        return [np.where(self.delta[i, :] == self.channel_min[i])[0] for i in range(len(self.channel_min))]
+        return np.amin(self.delta, axis=1)
