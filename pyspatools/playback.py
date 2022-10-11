2402f32a1b6efd5c22be3ee3a5ebdfbadde1d4fa
jiajun <eric1986_chn@yahoo.com>
2022-10-11 19:08:12 +0200

    Playback add block options

--- pyspatools/playback.py
+++ pyspatools/playback.py
@@ -1,14 +1,15 @@
+import time
 from typing import Optional
 from warnings import warn
 
 
-def pya_warning():
+def pya_warning() -> None:
     ms1 = "pya is not installed. Can't play audio."
     ms2 = "Please follow instruction in https://github.com/interactive-sonification/pya to install "
     warn(ms1 + ms2)
 
 
-def device_info():
+def device_info() -> dict:
     """
     Return a dictionary of device info detected by PyAudio
     """
@@ -17,12 +18,17 @@ def device_info():
         return find_device()
     except ImportError:
         pya_warning()
+        return {}
 
 
-def play(inputs: list, channels: list,
-         sr: int = 48000, bs: int = 256,
-         block: bool = True, num_channels: Optional[int] = None,
-         device_index: Optional[int] = None):
+def play(inputs: list,
+         channels: list,
+         sr: int = 48000,
+         bs: int = 256,
+         block: bool = True,
+         num_channels: Optional[int] = None,
+         device_index: Optional[int] = None
+         ) -> None:
     """
     Take a list of AudioSignal and a list of integer for the channel (1 base) of each AudioSignal is supposed to be played on,
     play it through device using pya's Aserver context manager.
@@ -32,14 +38,23 @@ def play(inputs: list, channels: list,
         from pya import Asig
 
         max_channel = num_channels or max(channels)
+        # TODO Add resampling if signal sr doesn't match playback sr
         with Aserver(sr=sr,
                      bs=bs,
                      device=device_index,
                      channels=max_channel
                      ) as input_audio_server:
-            for item, chan in zip(inputs, channels):
-                asig = Asig(item.sig, item.sr)
-                asig.play(server=input_audio_server, channel=chan - 1, block=block)
+            if not block:
+                durs = []
+                for item, chan in zip(inputs, channels):
+                    asig = Asig(item.sig, item.sr)
+                    asig.play(server=input_audio_server, channel=chan - 1, block=False)
+                    durs.append(asig.dur)
+                time.sleep(max(durs))
+            else:
+                for item, chan in zip(inputs, channels):
+                    asig = Asig(item.sig, item.sr)
+                    asig.play(server=input_audio_server, channel=chan - 1, block=True)
 
     except ImportError:
         pya_warning()
