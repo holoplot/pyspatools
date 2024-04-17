f20e0e380c1a697a502f6f1e017ca5593906d6da
Jiajun <eric1986_chn@yahoo.com>
2024-04-17 23:54:12 +0200

    [signal] Make to_mono static method

--- pyspatools/signal.py
+++ pyspatools/signal.py
@@ -169,7 +169,7 @@ class AudioSignal:
 
         return freqs, times, Zxxs
 
-    def spectrum(self, n: Optional[int] = None) -> np.ndarray:
+    def spectrum(self, n: int | None = None) -> np.ndarray:
         """
         Calculate the absolute spectrum of each channel
 
@@ -182,15 +182,17 @@ class AudioSignal:
             [np.abs(np.fft.rfft(self.sig[:, i], n=n)) for i in range(self.sig.shape[1])]
         )
 
-    def to_mono(self):
+    @staticmethod
+    def to_mono(sig: np.ndarray) -> np.ndarray:
         """
         Mix channels to mono signal.
         """
-        blend = np.ones(self.channels) / self.channels
-        if len(blend) != self.channels:
+        channels = sig.shape[1]
+        blend = np.ones(channels) / channels
+        if len(blend) != channels:
             raise AttributeError("len(blend) != self.channels")
         else:
-            self.sig = np.sum(self.sig * blend, axis=1)
+            return np.sum(sig * blend, axis=1)
 
     def latency(self, threshold: float = 1.0, offset=0) -> list:
         """
